1924b80708a0cd2012030226ec9ae50d789ede93
gabri <gabriel.desouza64@gmail.com>
2023-03-13 23:00:55 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SistemasDeTarefas.Models;
+using SistemasDeTarefas.Repositorios.interfaces;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SistemasDeTarefas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
+        public UsuarioController(IUsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<UsuarioModel>>> BuscarTodosUsuarios()
+        {
+            List<UsuarioModel> usuarios = await _usuarioRepositorio.BuscarTodosUsuarios();
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<UsuarioModel>>> BuscarPorId(int id)
+        {
+            UsuarioModel usuario = await _usuarioRepositorio.BuscarPorId(id);
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UsuarioModel>> Cadastrar([FromBody] UsuarioModel usuarioModel)
+        {
+            UsuarioModel usuario = await _usuarioRepositorio.Adicionar(usuarioModel);
+
+            return Ok(usuario);
+        }
+    }
+}
---
+++ Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemasDeTarefas.Data;
+using SistemasDeTarefas.Models;
+using SistemasDeTarefas.Repositorios.interfaces;
+
+namespace SistemasDeTarefas.Repositorios
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        private readonly SistemaTarefasDBContext _dbcontext;
+        public UsuarioRepositorio(SistemaTarefasDBContext sistemaTarefasDBContext)
+        {
+            _dbcontext = sistemaTarefasDBContext;
+        }
+        public async Task<UsuarioModel> BuscarPorId(int id)
+        {
+            return await _dbcontext.Usuarios.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<List<UsuarioModel>> BuscarTodosUsuarios()
+        {
+            return await _dbcontext.Usuarios.ToListAsync();
+        }
+        public async Task<UsuarioModel> Adicionar(UsuarioModel usuario)
+        {
+            await _dbcontext.Usuarios.AddAsync(usuario);
+            await _dbcontext.SaveChangesAsync();
+
+            return usuario;
+        }
+
+        public async Task<bool> Apagar(int id)
+        {
+            UsuarioModel usuarioPorId = await BuscarPorId(id);
+            if(usuarioPorId == null)
+            {
+                throw new Exception($"Usuaario para o ID: {id} não foi encontrado no banco de dados");
+            }
+
+            _dbcontext.Usuarios.Remove(usuarioPorId);
+            await _dbcontext.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<UsuarioModel> Atualizar(UsuarioModel usuario, int id)
+        {
+            UsuarioModel usuarioPorId = await BuscarPorId(id);
+            if(usuarioPorId == null)
+            {
+                throw new Exception($"Usuário para o ID: {id} não foi encontrado no banco de dados");
+            }
+
+            usuarioPorId.Nome= usuario.Nome;    
+            usuarioPorId.Email= usuario.Email;
+
+            _dbcontext.Usuarios.Update(usuarioPorId);
+            await _dbcontext.SaveChangesAsync();
+
+            return usuarioPorId;
+        }
+
+    }
+}
